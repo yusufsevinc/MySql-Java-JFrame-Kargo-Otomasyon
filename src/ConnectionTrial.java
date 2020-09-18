72379904932ba843dd344bd5c4b8a6f2b28c3ced
yusufsevinc <1yusufsevinc@gmail.com>
2020-09-18 21:54:05 +0300

    MySql JavaJFrame Kargo Otomasyon

--- src/ConnectionTrial.java
+++ src/ConnectionTrial.java
@@ -1,14 +1,19 @@
 
-import com.sun.xml.internal.bind.v2.runtime.output.SAXOutput;
+import java.awt.List;
+import static java.awt.image.ImageObserver.HEIGHT;
+import static java.lang.Math.E;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.naming.spi.DirStateFactory;
+import javax.swing.JOptionPane;
 
 public class ConnectionTrial {
 
@@ -17,7 +22,8 @@ public class ConnectionTrial {
     //password
     private String password = "";
     //dataBase name
-    private String dataBaseName = "demo";
+    private String dataBaseName = "kullanici";
+
     //server host
     private String host = "localhost";
     //server port
@@ -28,70 +34,19 @@ public class ConnectionTrial {
 
     //sql run query
     private Statement statement = null;
-    
+
     //prepared statement 
     private PreparedStatement preparedStatement = null;
-    
-    
-    
-    public void preparedEmployeeInfos(int userid , String username){
-        String query = "Select * From calisanlar where id > ?  and name like ?";
-        try {
-            preparedStatement = connection.prepareStatement(query);
-                    preparedStatement.setInt(1, userid);
-                    preparedStatement.setString(2, username);
-                  ResultSet rs =   preparedStatement.executeQuery();
-                  while (rs.next()) {
-                    int id = rs.getInt("id");
-                String name = rs.getString("name");
-                String surname = rs.getString("surname");
-                String email = rs.getString("email");
-                System.out.println("İD: " + id + "\n"
-                        + "Name : " + name + "\n"
-                        + "Surname: " + surname + "\n"
-                        + "Email: " + email);
-                
-            }
 
-        } catch (SQLException ex) {
-            Logger.getLogger(ConnectionTrial.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-    
-    
-
-    public void workingInfos() {
-        String query = "Select * from calisanlar where name like 'Y%'";
+    public void addUser(String name, String surName, String password) {
+        
 
         try {
             statement = connection.createStatement();
-            ResultSet rs = statement.executeQuery(query);
-
-            while (rs.next()) {
-                int id = rs.getInt("id");
-                String name = rs.getString("name");
-                String surname = rs.getString("surname");
-                String email = rs.getString("email");
-                System.out.println("İD: " + id + "\n"
-                        + "Name : " + name + "\n"
-                        + "Surname: " + surname + "\n"
-                        + "Email: " + email);
-
-            }
-
-        } catch (SQLException ex) {
-            Logger.getLogger(ConnectionTrial.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-    }//working 
-
-    public void addEmployee() {
-        try {
-            statement = connection.createStatement();
-            String addName = "Ayşe";
-            String addSurname = "sevinç";
-            String addEmail = "aysesevinc@gmail.com";
-            String addQuery = "Insert Into calisanlar (name , surname ,email) values (" + "'" + addName.toUpperCase() + "'," + "'" + addSurname.toUpperCase() + "'," + "'" + addEmail + "')";
+            String addNameUser = name;
+            String addSurnameUser = surName;
+            String addPasswordUser = password;
+            String addQuery = "Insert Into kullanicilar (userName , userSurname ,userPassword) values (" + "'" + addNameUser.toUpperCase() + "'," + "'" + addSurnameUser.toUpperCase() + "'," + "'" + addPasswordUser + "')";
             statement.executeUpdate(addQuery);
 
         } catch (SQLException ex) {
@@ -99,26 +54,233 @@ public class ConnectionTrial {
         }
     }//add Employee
 
-    public void updateEMployee() {
+    public void addAdministrator(String name, String password) {
         try {
             statement = connection.createStatement();
-            String updateQuery = "update calisanlar  set surname = 'SEVİNÇ' where id = 4 ";
-            statement.executeUpdate(updateQuery);
+            String addNameAdmin = name;
+            String addPasswordAdmin = password;
+            String addQuery = "Insert into yoneticiler (adminName , adminPassword) values (" + "'" + addNameAdmin.toUpperCase() + "'," + "'" + addPasswordAdmin.toUpperCase() + "')";
+            statement.executeUpdate(addQuery);
+        } catch (SQLException e) {
+        }
+
+    }
+
+    public void updateUser(String userName, String userSurname, String userPassword, String userPastPassword) {
+        String updateQuery = "update kullanicilar set userName = ? , userSurname = ? , userPassword = ? where userPassword = ? ";
+
+        try {
+            preparedStatement = connection.prepareStatement(updateQuery);
+            preparedStatement.setString(1, userName);
+            preparedStatement.setString(2, userSurname);
+            preparedStatement.setString(3, userPassword);
+            preparedStatement.setString(4, userPastPassword);
+
+            int row = preparedStatement.executeUpdate();
+
+            if (row == 1) {
+                JOptionPane.showMessageDialog(null, "Kullanıcı Bilgileri Değiştirildi. ", " ! Bilgilendirme", HEIGHT);
+
+            } else {
+                JOptionPane.showMessageDialog(null, "Kullanıcı Bulunamadı. \n"
+                        + "Bilgilerin Eksiksiz ve Doğru Olduğundan Emin olun ! ", " ! Bilgilendirme", HEIGHT);
+
+            }
         } catch (SQLException ex) {
             Logger.getLogger(ConnectionTrial.class.getName()).log(Level.SEVERE, null, ex);
         }
 
     }//update Employee
-    public void deleteEmployee(){
+
+    public void deleteUser(int id, String userName, String userSurname) {
+        String deleteQuery = "DELETE FROM kullanicilar WHERE id = ? and userName = ? and userSurname = ? ";
+
         try {
-            statement = connection.createStatement();
-            String deleteQuery = "delete from calisanlar where id >4";
-            int value = statement.executeUpdate(deleteQuery);
-            System.out.println(value + " employee deleted.");
+            preparedStatement = connection.prepareStatement(deleteQuery);
+            preparedStatement.setInt(1, id);
+            preparedStatement.setString(2, userName);
+            preparedStatement.setString(3, userSurname);
+
+            int row = preparedStatement.executeUpdate();
+            if (row == 1) {
+
+                JOptionPane.showMessageDialog(null, "Kullanıcı Silindi. ", " ! Bilgilendirme", HEIGHT);
+
+            } else {
+                JOptionPane.showMessageDialog(null, "Kullanıcı Bulunamadı. \n"
+                        + "Bilgilerin Eksiksiz ve Doğru Olduğundan Emin olun ! ", "! Bilgilendirme", HEIGHT);
+            }
+
         } catch (SQLException ex) {
             Logger.getLogger(ConnectionTrial.class.getName()).log(Level.SEVERE, null, ex);
         }
     }//delete employee
+
+    public String cargoS() {
+        String query = "select * from kargolar";
+        int id;
+        int aliciId;
+        int gondericiId;
+        String aliciAdres = null;
+        String gondericiAdres = null;
+        int kargoKG;
+
+        ArrayList<String> kargolar = new ArrayList<String>();
+
+        try {
+            statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(query);
+
+            while (rs.next()) {
+                id = rs.getInt("id");
+                aliciId = rs.getInt("aliciId");
+                gondericiId = rs.getInt("gondericiId");
+                aliciAdres = rs.getString("aliciAdres");
+                gondericiAdres = rs.getString("gondericiAdres");
+                kargoKG = rs.getInt("kargoKg");
+
+                kargolar.add(String.valueOf(id + "              "));
+                kargolar.add(String.valueOf(aliciId + "              "));
+                kargolar.add(String.valueOf(gondericiId + "              "));
+                kargolar.add(aliciAdres + "  ");
+                kargolar.add(gondericiAdres + "  ");
+                kargolar.add(String.valueOf(kargoKG + "   "));
+                kargolar.add("\n");
+
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionTrial.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return Arrays.toString(kargolar.toArray());
+
+    }
+
+    public String cargoS(int kargoId) {
+        String query = "select * from kargolar where id = ?";
+        int id;
+        int aliciId;
+        int gondericiId;
+        String aliciAdres = null;
+        String gondericiAdres = null;
+        int kargoKG;
+
+        ArrayList<String> kargolar = new ArrayList<String>();
+
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, kargoId);
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while (rs.next()) {
+                id = rs.getInt("id");
+                aliciId = rs.getInt("aliciId");
+                gondericiId = rs.getInt("gondericiId");
+                aliciAdres = rs.getString("aliciAdres");
+                gondericiAdres = rs.getString("gondericiAdres");
+                kargoKG = rs.getInt("kargoKg");
+
+                kargolar.add(String.valueOf(id + "              "));
+                kargolar.add(String.valueOf(aliciId + "              "));
+                kargolar.add(String.valueOf(gondericiId + "              "));
+                kargolar.add(aliciAdres + "  ");
+                kargolar.add(gondericiAdres + "  ");
+                kargolar.add(String.valueOf(kargoKG + "   "));
+                kargolar.add("\n");
+
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionTrial.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return Arrays.toString(kargolar.toArray());
+
+    }
+
+    public Boolean userLogin(String name, String password) {
+        String query = "select  * from kullanicilar";
+        Boolean kontrol = false;
+        try {
+            statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(query);
+
+            while (rs.next()) {
+                if (rs.getString("userName").equals(name) && rs.getString("userPassword").equals(password)) {
+                    kontrol = true;
+                    break;
+                }
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionTrial.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return kontrol;
+    }
+
+    public Boolean adminLogin(String name, String password) {
+        String query = "select  * from yoneticiler";
+        Boolean kontrol = false;
+        try {
+            statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(query);
+
+            while (rs.next()) {
+                if (rs.getString("adminName").equals(name) && rs.getString("adminPassword").equals(password)) {
+                    kontrol = true;
+                    break;
+                }
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionTrial.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return kontrol;
+    }
+
+    public void addCargo(int aliciId, int gondericiId, String aliciAdres, String gondericiAdres, String kargoKg) {
+        String query = "insert into  kargolar (aliciId ,gondericiId ,aliciAdres,gondericiAdres,kargoKg) values (" + '?' + "," + '?' + "," + '?' + "," + '?' + "," + '?' + ")";
+
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, aliciId);
+            preparedStatement.setInt(2, gondericiId);
+            preparedStatement.setString(3, aliciAdres);
+            preparedStatement.setString(4, gondericiAdres);
+            preparedStatement.setString(5, kargoKg);
+            int row = preparedStatement.executeUpdate();
+
+            if (row == 1) {
+                JOptionPane.showMessageDialog(null, "Kargo Ekleme Başarılı...", "! Bilgilendirme", HEIGHT);
+
+            } else {
+                JOptionPane.showMessageDialog(null, "Kargo Ekleme Başarısız. \n"
+                        + "Tekrar Deneyin.", "! Bilgilendirme", HEIGHT);
+
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionTrial.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void deleteCargo(int kargoId) {
+        String query = "DELETE  FROM kargolar WHERE id = ?";
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, kargoId);
+            int row = preparedStatement.executeUpdate();
+            if (row == 1) {
+                JOptionPane.showMessageDialog(null, "Kargo Silindi.", "! Dikkat", HEIGHT);
+
+            } else {
+                JOptionPane.showMessageDialog(null, "Kargo Silme Başarısız. \n"
+                        + "Bilgilerin Doğruluğundan Emin Olun.", " ! Dikkat", HEIGHT);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionTrial.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
 
     public ConnectionTrial() {
         //jbdc:mysql://localhost:3306/demo
@@ -132,19 +294,13 @@ public class ConnectionTrial {
             connection = DriverManager.getConnection(url, username, password);
             System.out.println("Connection succesfull");
         } catch (SQLException ex) {
-            System.out.println("Connection failed");
+            System.out.println("Connection failed" + ex);
         }
 
     }//connectrial constructer
 
     public static void main(String[] args) {
-        ConnectionTrial ct = new ConnectionTrial();
-//        ct.workingInfos();
-//        ct.addEmployee();
-//        ct.updateEMployee();
-//        ct.workingInfos();
-//         ct.deleteEmployee();
-ct.preparedEmployeeInfos(0,"N%");
+
     }//main
 
 }//ConnectionTrial 
